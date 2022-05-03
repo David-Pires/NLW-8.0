6f2d21226c7908dae946e4a242446af02faf1f2e
David Pires <developer.david@outlook.com>
2022-05-03 20:30:14 -0300

    feat: personalizing colors in TailWind

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -1,7 +1,20 @@
+import { ChatTeardropDots} from 'phosphor-react'
+
 export function Widget() {
   return (
     <div>
-      <h1>Widget</h1>
+      <button className="bg-brand-500
+                          rounded-full
+                          px-3
+                          h-12
+                          text-white 
+
+                          ">
+                            <ChatTeardropDots
+               className="w-6 h-6
+                          
+               "
+      /></button>
     </div>
   )
 }
