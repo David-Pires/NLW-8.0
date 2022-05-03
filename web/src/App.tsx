b0d5775f55f739cbae9addef902145ce385a19b6
David Pires <developer.david@outlook.com>
2022-05-03 20:19:13 -0300

    feat: phosphor-react

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,26 +1,9 @@
+import { Widget } from './components/Widget';
 
-interface ButtonProps {
-  text?: string 
-
-}
-function Button(props: ButtonProps) { 
-  return <button className={`
-  bg-purple-500 p-2
-  `}>{props.text ?? 'default'}</button>  
-  //Interrogação na interface torna a passagem de props opcional no componente
-}
-
-function App() {
+export function App() {
   return (
     <div>
-      <h1>Hello World</h1>
-
-      <Button text="enviar"/>
-      <Button text="mais"/>
-      <Button />
+      <Widget/>
     </div>
   )
 }
-
-        
-export default App
