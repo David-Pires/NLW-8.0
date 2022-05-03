941161e1207243b9c6cda58b414802715fafac54
David Pires <developer.david@outlook.com>
2022-05-03 19:13:02 -0300

    feat: considerações

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,16 +1,21 @@
 
-function Button(props) {
-  console.log(props.text)
-  return <button>I'm a button</button>
-  
+interface ButtonProps {
+  text?: string 
+
+}
+function Button(props: ButtonProps) { 
+  return <button>{props.text ?? 'esta frase aparece aqui vai aparecer aqui'}</button>  
+  //Interrogação na interface torna a passagem de props opcional no componente
 }
 
 function App() {
   return (
     <div>
       <h1>Hello World</h1>
+
       <Button text="enviar"/>
       <Button text="mais um"/>
+      <Button />
     </div>
   )
 }
